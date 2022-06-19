b0a074706f6eacf0c3df0831860fc03f5eca2cf0
Jerrin Wiley <jerrintwiley@gmail.com>
2022-06-18 21:43:31 -0500

    Added Methods

--- GTM_Trigger_Decoder.py
+++ GTM_Trigger_Decoder.py
@@ -1,59 +1,122 @@
 import json
-import csv
 import pandas as pd
+import sys
+import trigger_methods as tm
+import xlsxwriter
 
+# Import Arguments
+# total arguments
+n = len(sys.argv)
+ 
+# Arguments passed
+try:
+    file_name = sys.argv[1]
+except:
+    file_name = 'GTM-MVPNN2_workspace1000413.json'
 
-# Initializations
-file_name = "GTM-MVPNN2_workspace1000413.json"
+try:
+    export_file = sys.argv[2]
+except:
+    export_file = 'testing.xlsx'
 
+    
 # JSON to data frame
 df = pd.read_json(file_name)
 df2 = df.loc['trigger','containerVersion']
 df3 = pd.DataFrame(df2)
-df4 = df3.sort_values(by=['name'])
-row = 0
 
-while row < len(df4):
-    l = df4.loc[row, 'filter']
-    n = 1
+# print(df3)
+
+# customEventFilter parser
+for index, row in df3.iterrows():
+    customEventFilter_new = tm.event_parser(row['customEventFilter'], 'customEventFilter')
+    n = 0
     try:
-        for i in l:
-#             print(i)
-            string = str('')
-            string += i['type']
-            q = 1
-            for j in i['parameter']:
-        #         print(j)
-                if q < 2:
-                    string = j['value'] + ' '  + string
-                elif q > 2:
-                    pass
-                else:
-                    string += ' ' + j['value']
-                q += 1
-    #         print(string)
-            column_name = 'filter ' + str(n)
-    #         print(column_name)
-            df4.loc[row, column_name] = string
+        column_name = 'customEventFilter_new_%s' % (n)
+        df3.loc[index,'customEventFilter_new'] = customEventFilter_new[n]
+        n += 1
+    except:
+        pass
+    
+# autoEventFilter parser
+for index, row in df3.iterrows():
+    autoEventFilter_new = tm.event_parser(row['autoEventFilter'], 'autoEventFilter')
+    n = 0
+    try:
+        while n < len(autoEventFilter_new):
+            column_name = 'autoEventFilter_new_%s' % (n)
+            df3.loc[index,column_name] = autoEventFilter_new[n]
             n += 1
     except:
-#         print(row)
-#         print('missing')
         pass
-    row += 1
-row = 0
 
-while row < len(df4):
+# Filter parser
+for index, row in df3.iterrows():
+    filter_new = tm.filter_parser(row['filter'])
+    n = 0
     try:
-        string = ''
-        l = df4.loc[row, 'customEventFilter'][0]
-        k = df4.loc[row, 'customEventFilter'][0]['type']
-        i = 0
-        string = df4.loc[row, 'customEventFilter'][0]['parameter'][0]['value'] + ' ' + df4.loc[row, 'customEventFilter'][0]['type'] + ' ' + df4.loc[row, 'customEventFilter'][0]['parameter'][1]['value']
-        df4.loc[row, 'eventFilter'] = string
+        # print(filter_new)
+        # print(len(filter_new))
+        while n < len(filter_new):
+            column_name = 'Filter_new_%s' % (n)
+            df3.loc[index,column_name] = filter_new[n]
+            n += 1
     except:
         pass
-    row += 1
-df5 = df4.drop(['filter','fingerprint','autoEventFilter','waitForTags','checkValidation','waitForTagsTimeout','uniqueTriggerId', 'customEventFilter'], axis=1)
-df_final = df5.iloc[:, [0,1,2,3,4,9,7,8,5,6]]
-df_final.to_csv('testing2.csv')
+
+# Parameter Parser
+
+for index, row in df3.iterrows():
+    parameter_new = tm.parameter_parser(row['parameter'], row['type'])
+    # print(row['type'])
+    n = 0
+    # print(parameter_new)
+    try:
+        df3.loc[index,'parameter_new'] = parameter_new
+    except: 
+        pass
+
+# print(df3.loc[0])
+
+# clean up columns and reorder
+df4 = df3.drop(['filter','fingerprint','autoEventFilter','waitForTags','checkValidation','waitForTagsTimeout','uniqueTriggerId', 'customEventFilter'], axis=1)
+last_col = len(df4.loc[0])
+order = []
+for i in range(last_col):
+    if i != 5 and i != 6:
+        order.append(i)
+order += [5,6]
+
+# print(order)
+df_final = df4.iloc[:, order]
+
+
+#Format workbook
+# Create a Pandas Excel writer using XlsxWriter as the engine.
+writer = pd.ExcelWriter(export_file, engine='xlsxwriter')
+
+# Convert the dataframe to an XlsxWriter Excel object. Turn off the default
+# header and index and skip one row to allow us to insert a user defined
+# header.
+df_final.to_excel(writer, sheet_name='Sheet1', startrow=1, header=False, index=False)
+
+# Get the xlsxwriter workbook and worksheet objects.
+workbook = writer.book
+worksheet = writer.sheets['Sheet1']
+
+# Get the dimensions of the dataframe.
+(max_row, max_col) = df_final.shape
+
+# Create a list of column headers, to use in add_table().
+column_settings = []
+for header in df_final.columns:
+    column_settings.append({'header': header})
+
+# Add the table.
+worksheet.add_table(0, 0, max_row, max_col - 1, {'columns': column_settings})
+
+# Make the columns wider for clarity.
+worksheet.set_column(0, max_col - 1, 12)
+
+# Close the Pandas Excel writer and output the Excel file.
+writer.save()
---
+++ trigger_methods.py
@@ -0,0 +1,170 @@
+# Test filters
+filter = [
+    {
+        "type": "CONTAINS",
+        "parameter": [
+            {
+                "type": "TEMPLATE",
+                "key": "arg0",
+                "value": "{{Page URL}}"
+            },
+            {
+                "type": "TEMPLATE",
+                "key": "arg1",
+                "value": "docs.couchbase.com"
+            }
+        ]
+    },
+    {
+        "type": "MATCH_REGEX",
+        "parameter": [
+            {
+                "type": "TEMPLATE",
+                "key": "arg0",
+                "value": "{{element id}}"
+            },
+            {
+                "type": "TEMPLATE",
+                "key": "arg1",
+                "value": "(yesBtn)|(noBtn)"
+            }
+        ]
+    }
+]
+
+customEventFilter = [
+    {
+        "type": "EQUALS",
+        "parameter": [
+            {
+                "type": "TEMPLATE",
+                "key": "arg0",
+                "value": "{{_event}}"
+            },
+            {
+                "type": "TEMPLATE",
+                "key": "arg1",
+                "value": "customEvent"
+            }
+        ]
+    }
+]
+
+parameter = [
+    {
+        'type': 'BOOLEAN', 
+        'key': 'useOnScreenDuration', 
+        'value': 'false'
+    }, 
+    {
+        'type': 'BOOLEAN', 
+        'key': 'useDomChangeListener', 
+        'value': 'true'
+    }, 
+    {
+        'type': 'TEMPLATE', 
+        'key': 'elementSelector', 
+        'value': 'div.mktoForm'
+    }, 
+    {
+        'type': 'TEMPLATE', 
+        'key': 'firingFrequency', 
+        'value': 'ONCE'
+    }, 
+    {
+        'type': 'TEMPLATE', 
+        'key': 'selectorType', 
+        'value': 'CSS'
+    }, 
+    {
+        'type': 'TEMPLATE', 
+        'key': 'onScreenRatio', 
+        'value': '50'
+    }
+]
+
+
+
+# Returns list of formatted filter conditions
+def filter_parser(filter):
+    # print(len(filter))
+    list = []
+    try:
+        for i in filter:
+            # print(i)
+            variable = i['parameter'][0]['value']
+            condition = i['type']
+            value = i['parameter'][1]['value']
+            # print(type(list))
+            string = "%s %s '%s'" % (variable, condition, value)
+            # print(string)
+            list.append(string)
+    except:
+        pass
+
+    return list
+
+# Returns list of formatted filter conditions
+def event_parser(custom_event_filter, filter_type):
+    # print(len(filter))
+    list = []
+    try:
+        for i in custom_event_filter:
+            # print(i)
+            event = i['parameter'][0]['value']
+            condition = i['type']
+            value = i['parameter'][1]['value']
+            # print(type(list))
+            string = "%s %s '%s'" % (event, condition, value)
+            # print(string)
+            list.append(string)
+    except:
+        pass
+    return list
+
+# Returns string of formatted parameter conditions
+def parameter_parser(parameter,type):
+    elementVisibility_dict = {
+        'selectorType': 'Selection Method: ',
+        'elementSelector': 'Element Selector: ',
+        'firingFrequency' : 'When to fire this trigger: ',
+        'onScreenRatio' : 'Minimum Percent Visible: ',
+        'useOnScreenDuration' : 'Set minimum on-screen duration: ',
+        'useDomChangeListener' : 'Observe DOM changes: ',
+    }
+
+    string = ''
+    if type == 'ELEMENT_VISIBILITY':
+        try:
+            for j, i in enumerate(parameter):
+                key = i['key']
+                value = i['value']
+                try:
+                    cat = elementVisibility_dict[key]
+                    if j == 0:
+                        string += '%s%s' % (cat,value)
+                    else:
+                        string += '\n%s%s' % (cat,value)
+                except:
+                    if j == 0:
+                        string += '%s EQUALS %s' % (key,value)
+                    else:
+                        string += '\n%s EQUALS %s' % (key,value)
+                # print(value)
+        except:
+            pass
+    else:
+        try:
+            for i in parameter:
+                key = i['key']
+                value = i['value']
+                # print(value)
+                string += '\n%s EQUALS %s' % (key,value)
+        except:
+            pass
+    return string
+
+# # Function tests
+# print(parameter_parser(parameter, 'elementVisibility'))
+# print(filter_parser(filter)[0])
+# print(event_parser(customEventFilter, 'customEvent')[0])
